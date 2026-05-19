ab8813cdc2e34817e0334e84f415a3ca1a816b7d
SouthWind <15821745523@163.com>
2026-05-19 11:17:12 +0800

    使用内置kv

--- api/getUserNotes.js
+++ api/getUserNotes.js
@@ -1,11 +1,8 @@
-import { JSONFilePreset } from 'lowdb/node'
-const db = await JSONFilePreset('db.json', { users: [], notes: [] })
+import { kv } from '@vercel/kv'
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  if (req.method !== 'GET') return res.status(405).end()
-
   const { userId } = req.query
-  const list = db.data.notes.filter(n => n.userId == userId)
-  res.json({ code: 200, data: list })
+  const data = await kv.get(`notes:${userId}`) || []
+  return res.json({ code: 200, data })
 }
